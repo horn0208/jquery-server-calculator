1875309c2d31713fa8cfa5c653319178da2463c5
Liz Horn <liz.mpls@gmail.com>
2022-04-28 15:47:40 -0500

    calculateIt function in server works with addition

--- server/server.js
+++ server/server.js
@@ -7,13 +7,7 @@ const router = express.Router();
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({extended: true})); // needed for POST
 app.use('/calc', router);
-//route POST from client
-router.post('/', (req, res)=>{
-    console.log('POST in server:', req.body);
-    //push into history array
-    //run calculation function
-    res.sendStatus(200);
-})
+
 //globals
 const calcHistory = []; //stores all previous calculations
 const port = 5001;
@@ -21,7 +15,27 @@ const port = 5001;
 app.listen(port,()=>{
     console.log('server up on:', port);
 })
+
 //calculation functions
+function calculateIt(toCalc){
+    console.log('in calculateIt, toCalc:', toCalc);
+    if(toCalc.operandClicked === 'plusButton'){
+        let answer = Number(toCalc.num1) + Number(toCalc.num2);
+        console.log('answer', answer);
+    }
+}
+
+//POST route from client
+router.post('/', (req, res)=>{
+    // console.log('POST in server:', req.body);
+    //store req.body object in a variable and pass it to calculatIt function
+    let toCalc = req.body;
+    calculateIt(toCalc);
+
+    //push into history array
+    //run calculation function
+    res.sendStatus(200);
+})
 
 //export
 module.exports = router;
