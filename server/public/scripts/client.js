1251d6b5e7de3fe6bf585901a30b33101a9a14ca
Liz Horn <liz.mpls@gmail.com>
2022-04-28 10:47:18 -0500

    click handlers working. submitOperand updates toBeCalculated object with id of button clicked

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,27 @@
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('jq');
+    //click handlers
+    $('#submitCalcButton').on('click', submitCalc);
+    $('.operandButton').on('click', submitOperand);
+
+    //display history function
+
+}
+//create an object to hold input vals.
+const toBeCalculated = {
+    num1: 'nothing yet',
+    num2: 'nothing yet',
+    operandClicked: 'nothing yet'
+}
+//function to take submitted operand and store its id in toBeCalculated object
+function submitOperand(){
+    console.log('in submitOperand:', this.id);
+    console.log('tobeCalculated before:', toBeCalculated);
+    toBeCalculated.operandClicked = this.id;
+    console.log('toBeCalculated after:', toBeCalculated);
+}
+
+function submitCalc(){
+    console.log('in submitCalc:');
 }
